9fab85dd7ad8aec4b0b78d668af9b4acb597b1d5
Attila <darkhan42@gmail.com>
2014-10-31 08:54:10 +0100

    Create olvass.el

---
+++ olvass.el
@@ -0,0 +1,9 @@
+"Prímek" program 
+Egy matektanárokat segítő programot próbáltam írni pythonban. 
+A program egy létező problémára reflektál: kevés a példa a matematika tankönyvekben, 
+így kell valami, aminek segítségével könnyen, gyorsan lehet feladatot generálni. 
+A feladat egyszerű: prímtényezős felbontása két számnak, legnagyobb közös osztó, 
+és legkisebb közös többszörös megtalálása. 
+A következő fejlesztési lépcső a program hordozhatóvá tétele lesz, 
+ugyanis minél felhasználóbarátabbnak kell majd lennie. 
+(iskolai környezet gépparkja, órán való használata ne akadályozza az előadást)
